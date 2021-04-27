1d45cda24b61a559497146ef8e280dbb75942988
joanar2021daw2 <joanar2021daw2@protonmail.com>
2021-04-27 11:33:27 +0200

    Versió 5:Entitat Pas, procés linkejem a angular. Fusió amb plantilla

--- src/main/java/cat/xtec/ioc/Proccontrol/config/SecurityConfig.java
+++ src/main/java/cat/xtec/ioc/Proccontrol/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package cat.xtec.ioc.Proccontrol.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,6 +20,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+          .withUser("user1").password(passwordEncoder().encode("user1Pass"))
+          .authorities("ROLE_USER");
     }
 
     @Override
